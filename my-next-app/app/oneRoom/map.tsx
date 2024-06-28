31d29591d2ed3da95411f987ed1f1a3b6026505a
bacem-slimi <slimii.bacem@gmail.com>
2024-06-28 23:01:46 +0100

    some functionalities

--- my-next-app/app/oneRoom/map.tsx
+++ my-next-app/app/oneRoom/map.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import { Box } from '@mui/material';
-<<<<<<< HEAD
 interface mapProps {
   location: string;
 }
 const Map = ({ location }:mapProps) => {
   const mapUrl = `https://www.google.com/maps/embed/v1/place?key=YOUR_GOOGLE_MAPS_API_KEY&q=${encodeURIComponent(location)}`;
-=======
-
-const Map = ({ location }:any) => {
-  const mapUrl= `https://www.google.com/maps/embed/v1/place?key=YOUR_GOOGLE_MAPS_API_KEY&q=${encodeURIComponent(location)}`;
->>>>>>> 493f5051fd2b685b85cd3e9a284410b7871db446
 
   return (
     <Box
