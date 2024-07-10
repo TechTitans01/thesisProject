998d25c34a903e317250e2e7e6229089ca0a9493
Ahmedgafsi <ahmed.gafsi.2001@gmail.com>
2024-07-10 11:49:21 +0100

    done send message in gmail reclamation

--- server/database/routes/reclamtion.js
+++ server/database/routes/reclamtion.js
@@ -1,10 +1,12 @@
 const router= require("express").Router()
 
-const {getAllReclamation,getOneReclamationByEmail,sendReclamation}=require("../controller/reclamation.js")
+const {getAllReclamation,getOneReclamationByEmail,sendReclamation, sendReclamtionGmail}=require("../controller/reclamation.js")
 
 router.get("/getone/", getOneReclamationByEmail);
 router.get("/getall", getAllReclamation);
 router.post("/send",sendReclamation);
+router.post("/send/gmail",sendReclamtionGmail);
+
 
 
 module.exports=router
